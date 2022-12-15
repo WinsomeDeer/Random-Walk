3c35ed6429e43003b29f5028940d73a65eb52e00
Patrik <103080404+WinsomeDeer@users.noreply.github.com>
2022-12-15 02:13:43 +0000

    Update randomwalk.py

--- randomwalk.py
+++ randomwalk.py
@@ -1,3 +1,17 @@
 import numpy as np
 import random
 import matplotlib as plt
+# Class for the random walk.
+class Random_walk:
+  # Constructor.
+  def __init__(self, n, p, start):
+    self.prob = p
+    self.steps = n
+    self.total = start
+  # Compute one step.
+  def one_step(self):
+    if random.choice([-1,1]) == 1:
+      self.total += 1
+    else:
+      self.total -= 1
+  
