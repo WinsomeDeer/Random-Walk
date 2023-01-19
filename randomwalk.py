e05026c88d14a21ee8c8a9763bbebe03bdef00af
Patrik <103080404+WinsomeDeer@users.noreply.github.com>
2023-01-19 23:51:19 +0000

    Update randomwalk.py

--- randomwalk.py
+++ randomwalk.py
@@ -1,26 +1,38 @@
 import numpy as np
-import random
-import matplotlib as plt
-# Class for the random walk.
-class Random_walk:
-    # Constructor.
-    def __init__(self, n, pr, start):
-        self.prob = pr
-        self.end = n
-        self.total = start
-        self.steps = 0
-    # Compute one walk.
-    def walk(self):
-        self.steps = 0
-        while(self.steps < self.end):
-            self.total += np.random.choice([1,-1], p = [self.prob, 1-self.prob])
-            self.steps += 1
-        return self.total
-    # Compute the number of steps till a certain total is hit.
-    def walk_till(self, target):
-        self.total = 0
-        self.steps = 0
-        while(self.total != target):
-            self.total += np.random.choice([1,-1], p = [self.prob, 1-self.prob])
-            self.steps += 1
-        return self.steps
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+def random_walk(N, pr):
+    steps = np.random.choice([-1,1], N, pr)
+    total = np.cumsum(steps)
+    return steps, total
+
+# Random walk example
+N = 100
+p = 0.5
+
+X, Y = random_walk(N, p)
+
+# General figure options
+fig = plt.figure(figsize=(15, 7))
+ax = plt.axes(xlim=(0, N), ylim=(np.min(Y), np.max(Y)))
+line, = ax.plot([], [], lw=2)
+ax.set_title('2D Random Walk', fontsize=18)
+ax.set_xlabel('Steps', fontsize=16)
+ax.set_ylabel('Value', fontsize=16)
+ax.tick_params(labelsize=12)
+
+def init():
+    line.set_data([], [])
+    return line,
+
+x, y = [], []
+
+def animate(i):
+    x.append(i)
+    y.append(Y[i])
+    line.set_data(x, y)
+    return line,
+
+ani = animation.FuncAnimation(fig, animate, init_func = init, frames = 500, interval = 1000, blit = True)
+plt.show()
